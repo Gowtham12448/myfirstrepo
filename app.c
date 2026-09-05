15b9fe6ba5a757797b69c8373b435dc28bef7cc9
Gowtham12448 <70027172+Gowtham12448@users.noreply.github.com>
2026-09-06 04:21:05 +0530

    Add basic user input program in app.c

---
+++ app.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char name[50];
+    int age;
+
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("\nHello %s\n", name);
+    printf("You are %d years old.\n", age);
+
+    if (age >= 18) {
+        printf("You are an adult.\n");
+    } else {
+        printf("You are a minor.\n");
+    }
+
+    return 0;
+}
