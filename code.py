e60f6314b39167ef79aab79b81e85a07d6b94299
Gowtham12448 <70027172+Gowtham12448@users.noreply.github.com>
2026-09-06 12:07:06 +0530

    Change age check from 18 to 19 for adulthood

--- code.py
+++ code.py
@@ -6,7 +6,7 @@ age = int(input("Enter your age: "))
 print("Hello Hi", name)
 print("You are", age, "years old")
 
-if age >= 18:
+if age >= 19:
     print("Since you are above 18 ,You are an adult.")
 else:
     print("Since you are below 18 ,You are a minor.")
